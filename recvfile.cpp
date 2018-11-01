19ceab88d0a6144da0778e7f88ac8dae297dd139
kelvinkristian <kelvinkristian4@gmail.com>
2018-11-01 19:24:58 +0700

    first commit

---
+++ recvfile.cpp
@@ -0,0 +1,19 @@
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <thread>
+#include <iostream>
+
+//#include "helpers.h"
+
+#define WAITING_TIME 3000
+
+using namespace std;
+
+int socket_fd;
+struct sockaddr_in server_addr, client addr;
+
+int main(int argc, char * argv[]) {
+
+    return 0;
+}
